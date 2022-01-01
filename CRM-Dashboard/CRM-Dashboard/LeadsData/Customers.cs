fdd5e0f883c8d3e42af778afd0815b8a4022b1e7
Selvan-DEV <selvandevgit@gmail.com>
2022-01-01 21:35:50 +0530

    new changes

---
+++ CRM-Dashboard/CRM-Dashboard/LeadsData/Customers.cs
@@ -0,0 +1,36 @@
+﻿using CRM_Dashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM_Dashboard.LeadsData
+{
+    public class Customers : ICustomersData
+    {
+        public CustomerPersonalData AddCustomer(CustomerPersonalData customerPersonalData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomer(CustomerPersonalData customerPersonalData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerPersonalData EditCustomer(CustomerPersonalData customerPersonalData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerPersonalData GetCustomer(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerPersonalData> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CRM-Dashboard/CRM-Dashboard/LeadsData/ICustomersData.cs
@@ -0,0 +1,21 @@
+﻿using CRM_Dashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM_Dashboard.LeadsData
+{
+    interface ICustomersData
+    {
+        List<CustomerPersonalData> GetCustomers();
+
+        CustomerPersonalData GetCustomer(int Id);
+
+        CustomerPersonalData AddCustomer(CustomerPersonalData customerPersonalData);
+
+        void DeleteCustomer(CustomerPersonalData customerPersonalData);
+
+        CustomerPersonalData EditCustomer(CustomerPersonalData customerPersonalData);
+    }
+}
